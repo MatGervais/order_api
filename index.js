75234cc8c1c370107c3fa4b6d978b95718bec240
Mathieu Gervais <matgervais96@gmail.com>
2022-08-19 16:04:40 +0200

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express();
+
+const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -8,6 +10,6 @@ app.use(bodyParser.json())
 
 app.use('/api/medication', require('./src/routes/medication'))
 
-app.listen(5000, ()=>{
-    console.log("listening");
+app.listen(port, ()=>{
+    console.log("listening on " + port);
 })
