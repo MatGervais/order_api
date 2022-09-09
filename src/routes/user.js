f13bef6ba144bd1bd05401456fa4a92dc299fc1f
Mathieu Gervais <matgervais96@gmail.com>
2022-09-09 15:33:41 +0200

    Adding user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,6 +8,7 @@ const {user} = new PrismaClient()
 router.get('/', validateToken, async (req, res)=>{
     const users = await user.findMany({
         select:{
+            id:true,
             email:true,
             lastname:true,
             firstname:true,
@@ -42,6 +43,33 @@ router.get('/:id', validateToken, async (req, res)=>{
     }
 
     res.status(200).json({user:userExists})
+})
+
+router.put('/:id', validateToken, async (req, res)=>{
+    const userId = parseInt(req.params.id)
+    const userDatas = req.body
+
+    const userExists = await user.findUnique({
+        where:{
+            id: userId
+        }
+    })
+
+    if(!userExists){
+         return res.status(400).json({success: false, message:"Cet utilisateur n'existe pas"})
+    }
+    try{
+    const modifyUser = await user.update({
+        where:{
+            id: userId
+        },
+        data: userDatas
+        
+    })
+    res.status(200).json({success: true, message:"Informations sauvegardées", modifyUser})
+    } catch(err){
+        res.status(400).json({success: false, message:"Échec lors de la mise à jour des informations"})
+    }
 })
 
 
