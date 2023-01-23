db0507ed4c340507e9eb49b22c605be78fc230a6
Mathieu Gervais <matgervais96@gmail.com>
2023-01-23 12:24:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const bodyParser = require('body-parser')
 //     credentials:true,
 // }))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHheader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, Authorization');
   // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   // res.header("Access-control-Allow-Credentials", true)
